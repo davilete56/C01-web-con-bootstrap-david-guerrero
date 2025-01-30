0c2b0005ad8fb27fe74eb6dde61cea1a43a063c3
DavidGueRod <davidguerrerorodriguez56@gmail.com>
2025-01-30 09:53:38 +0100

    Modify index.js: add new arguments to use the middleware of counting the number of visits to the page.

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,53 @@ const path = require('path');
 const app = express();
 
 
+//! Contador de visitas
+const contadorVisitas = {
+    inicio: 0,
+    productos: 0,
+    tiendas: 0,
+    servicios: 0,
+    ofertas: 0,
+    nosotros: 0,
+    contacto: 0
+};
 
-//# Middleware para usar archivos estáticos
+const contarVisitas = (page) => (req, res, next) => {
+    contadorVisitas[page]++;
+    console.log(`Visitas a la página de ${page}: ${contadorVisitas[page]}`);
+    next();
+};
+
+//! Middleware para usar archivos estáticos
 app.use(express.static(path.join(__dirname, '../public')));
 
-//# Rutas
-app.get('/', (req, res) => {
+//! Rutas
+//# Añadimos el middleware contarVisitas a cada ruta para contar las visitas que se hacen a cada página
+app.get('/', contarVisitas('inicio'), (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/productos', (req, res) => {
+app.get('/productos', contarVisitas('productos'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/productos.html'));
 });
 
-app.get('/tiendas', (req, res) => {
+app.get('/tiendas', contarVisitas('tiendas'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/tiendas.html'));
 });
 
-app.get('/servicios', (req, res) => {
+app.get('/servicios', contarVisitas('servicios'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/servicios.html'));
 });
 
-app.get('/ofertas', (req, res) => {
+app.get('/ofertas', contarVisitas('ofertas'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/ofertas.html'));
 });
 
-app.get('/sobre-nosotros', (req, res) => {
+app.get('/sobre-nosotros', contarVisitas('nosotros'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/sobre-nosotros.html'));
 });
 
-app.get('/contacto', (req, res) => {
+app.get('/contacto', contarVisitas('contacto'), (req, res) => {
     res.sendFile(path.join(__dirname, '../components/contacto.html'));
 });
 
